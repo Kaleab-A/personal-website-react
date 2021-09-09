5a1ad3832eb7c2f3bffb1594971a4c6151f2cce2
Kaleab Asfaw <50099796+Kaleab-A@users.noreply.github.com>
2021-09-09 17:52:30 +0200

    Update CommentSection.js

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,64 +1,65 @@
 import React, { Component } from "react";
-import AddComments from "./addComments";
+import { useEffect, useState } from "react";
 
-class CommentSection extends Component {
-	handleSubmit = () => setTimeout(() => window.location.reload(), 1000);
-	render() {
-		return (
-			<div>
-				<div className="card">
-					<div className="card-header" style={{ textAlign: "center" }}>
-						<h4>Latest Comments</h4>
+const AddComments = () => {
+	useEffect(() => {
+		const script = document.createElement("script");
+		script.src =
+			"https://spreadsheets.google.com/feeds/cells/11wjCNQwDPQ3c_6zesa7B2eHip4cHSiO57oTVdTf_o5c/1/public/values?alt=json-in-script&callback=doData";
+			// "https://sheets.googleapis.com/v4/spreadsheets/11wjCNQwDPQ3c_6zesa7B2eHip4cHSiO57oTVdTf_o5c/1/public/values?alt=json-in-script&callback=doData"
+		script.async = true;
+		script.type = "module";
+		document.body.appendChild(script);
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
+	const [commentMap, setCommentMap] = useState(null);
+	var spData = JSON.parse(localStorage.getItem("dataSP"));
+	var commentTable;
+	var renderCommentList = [{"name": "Kaleab Azezew", "time": "9/9/2021 1:24:08", "comment":"It could show (load) comments temporarily because the Sheets v3 API has been turned down."}];
+	function updateComments() {
+		commentTable = [];
+		function readData() {
+			var data = spData;
+			var rowData = [];
+			// for (var r = 0; r < data.length; r++) {
+			// 	var cell = data[r]["gs$cell"];
+			// 	var val = cell["$t"];
+			// 	if (cell.col == 1) {
+			// 		commentTable.push(rowData);
+			// 		rowData = [];
+			// 	}
+			// 	rowData.push(val);
+			// }
+			commentTable.push(rowData);
+		}
+		readData();
+		setTimeout(() => {
+			for (let index = 2; index < commentTable.length; index++) {
+				const element = commentTable[index];
+				renderCommentList.push({ time: element[0], name: element[1], comment: element[2] });
+			}
+			setCommentMap(
+				renderCommentList.map((singleComment) => (
+					<div className="comments" key={singleComment}>
+						<h5 className="card-title">
+							{singleComment.name} <span>({singleComment.time})</span>
+						</h5>
+						<p className="card-text">{singleComment.comment}</p>
 					</div>
-					<div className="card-body" id="commentDiv">
-						<AddComments />
-					</div>
-					<div className="card-footer">
-						<div className="row">
-							{/* <div className="col-lg-3"></div> */}
-							<div className="col-lg-8" style={{ marginLeft: 250 }}>
-								<button className="btn btn-primary" type="button" data-toggle="modal" data-target="#addComment">
-									Add Comment
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="modal fade" id="addComment" tabIndex="-1" role="dialog" aria-labelledby="addCommentLabel" aria-hidden="true">
-					<div className="modal-dialog" role="document">
-						<div className="modal-content">
-							<div className="modal-header">
-								<h5 className="modal-title">Add Comment</h5>
-								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-							<div className="modal-body">
-								<form
-									name="gform"
-									id="gform"
-									encType="text/plain"
-									action="https://docs.google.com/forms/d/e/1FAIpQLSdCIMJvXAFV1qKlKZEaXOKaG1lwKYZJzWBiHwZumuludb_gkw//formResponse?"
-									target="hidden_iframe"
-								>
-									<input className="form-control" type="text" name="entry.1603982339" id="entry.1603982339" placeholder="User Name" />
-									<br />
-									<input className="form-control" type="text" name="entry.543864157" id="entry.543864157" placeholder="Comment Here..." />
-									<hr />
-									<div style={{ textAlign: "right" }}>
-										<button type="submit" value="Submit" className="btn btn-primary" id="submitComment" onClick={this.handleSubmit}>
-											Send Message
-										</button>
-									</div>
-									<iframe name="hidden_iframe" id="hidden_iframe" style={{ display: "none" }}></iframe>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
+				))
+			);
+		}, 50);
 	}
-}
+	setTimeout(() => {
+		updateComments();
+	}, 2000);
+	setTimeout(() => {
+		console.log("Reloading...");
+		window.location.reload();
+	}, 50 * 1000);
+	return commentMap ? <div>{commentMap}</div> : <p>Loading</p>;
+};
 
-export default CommentSection;
+export default AddComments;
